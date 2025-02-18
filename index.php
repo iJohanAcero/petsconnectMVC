de961a1b20200aa5e0b628113e424168469cee7a
JohanAcero <johanacero8@gmail.com>
2025-02-18 12:56:25 -0500

    Le agregue solamente el icono de petsconnect en la barra de navegador se puede apreciar.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="es">
 
 <head>
+    <link rel="shortcut icon" href="Public/images/icono.png" />
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"> <!-- Llamamos a la librería de iconos --> 
