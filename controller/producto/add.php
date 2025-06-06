ce900868c827d74cb380be0ef9da51a944545f3d
KatheZmb <katherojas.2805@gmail.com>
2025-06-06 15:31:54 -0500

    cambio de las variables a minusculas (tipo_producto)

--- controller/producto/add.php
+++ controller/producto/add.php
@@ -5,12 +5,12 @@ require_once ('../../Model/producto/ProductoModel.php');
 if($_POST){
     $ModeloProducto = new Productos();
     $Nombre=$_POST['nombre'];
-    $TipoProducto=$_POST['TipoProducto'];
+    $tipo_producto=$_POST['tipo_producto'];
     $Descripcion=$_POST['descripcion'];
     $Precio=$_POST['precio'];
     $Cantidad=$_POST['cantidad_disponible'];
 
-    $ModeloProducto->add($Nombre, $TipoProducto, $Descripcion, $Cantidad, $Precio);
+    $ModeloProducto->add($Nombre, $tipo_producto, $Descripcion, $Cantidad, $Precio);
 }else{
     header("Location: view/producto/ProductoView.php");
 }
