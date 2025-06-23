a9d303fafbe892cc2a253bca139f14d8f2ae662e
KatheZmb <katherojas.2805@gmail.com>
2025-06-23 00:03:40 -0500

    Ajuste de rutas

---
+++ view/fundacion/FundacionView.php
@@ -0,0 +1,93 @@
+<?php
+//VALIDAR SESIÓN//
+require_once("../Model/fundacion/FundacionModel.php");
+$Modelo = new Fundacion();
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD_Fundacion</title>
+</head>
+
+<body>
+    <div class="crud">
+        <h2 class="titulo">CRUD de Fundaciones</h2>
+
+        <a class="btn-añadir" id="openModal">
+            <i class=" uil uil-plus-circle"></i> <span>Añadir Fundación</span>
+        </a>
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Nit Fundación</th>
+                <th>id_usuario</th>
+                <th>id_perfil</th>
+                <th>Acciones</th>
+            </tr>
+
+        </thead>
+        <tbody>
+            <?php
+            $Fundacion = $Modelo->getFundacion();
+            if ($Fundacion !== null) {
+                foreach ($Fundacion as $registroFundacion) {
+            ?>
+                    <tr>
+                        <td><?php echo $registroFundacion['nit_fundacion']; ?></td>
+                        <td><?php echo $registroFundacion['id_usuario']; ?></td>
+                        <td><?php echo $registroFundacion['id_perfil']; ?></td>
+                        <td>
+                            <a class="edit">
+                                <i class=" uil uil-pen" style="cursor: pointer;"></i>
+                            </a>
+                            <a  class="delete">
+                                <i class="uil uil-trash-alt" style="cursor: pointer;"></i>
+                            </a>
+                        </td>
+                    </tr>
+            <?php
+                }
+            } else {
+                echo "<tr><td colspan='4'>No hay fundaciones registradas.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+
+    <!-- ================================= HTML DEL FORMULARIO DE REGISTRO  ======================================= -->
+    <!----------- MODAL DEL BOTON DE AÑADIR REGISTRO -------------->
+
+    <div id="modal-fundacion" class="modal" style="display: none;">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2 class="titulo-modal">¡ Registro Nueva Fundación !</h2>
+
+            <!-------------- FORMULARIO FUNDACIÓN ------->
+
+            <form action="../../controller/fundacion/FundationController.php" method="POST" class="form-modal">
+
+                <label for="nit_fundacion">Nit Fundación:</label>
+                <input class="input-modal" type="text" id="nit_fundacion" name="nit_fundacion" required>
+
+                <label for="nombre_fundacion">Nombre fundación:</label>
+                <input class="input-modal" type="text" id="nombre_fundacion" name="nombre_fundacion" required>
+
+                <label for="id_usuario">ID Usuario:</label>
+                <input class="input-modal" type="text" id="id_usuario" name="id_usuario" required>
+
+                <label for="id_perfil">ID Perfil:</label>
+                <input class="input-modal" type="text" id="id_perfil" name="id_perfil" required>
+
+                <input type="hidden" name="action" value="add">
+                <button class="btn-añadir" type="submit">Guardar</button>
+            </form>
+
+        </div>
+    </div>
+</body>
+
+</html>
