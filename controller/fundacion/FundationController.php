434f185b89347be6c2aae32642441730e4eb65bd
KatheZmb <katherojas.2805@gmail.com>
2025-06-23 00:00:58 -0500

    prueba de controlador resumido en if else

--- controller/fundacion/FundationController.php
+++ controller/fundacion/FundationController.php
@@ -1,50 +1,70 @@
 <!-- AGREGAR -->
-<?php
+<!-- <?php
 
-require_once('../Modelo/FundacionModel.php');
+// require_once('../Modelo/FundacionModel.php');
 
-if ($_POST) {
-    $ModeloFundacion = new Fundacion();
-    $nit_fundacion = $_POST['nit_fundacion'];
-    $id_usuario = $_POST['id_usuario'];
-    $id_perfil = $_POST['id_perfil'];
+// if ($_POST) {
+//     $ModeloFundacion = new Fundacion();
+//     $nit_fundacion = $_POST['nit_fundacion'];
+//     $nombre_fundacion = $_POST['nombre_fundacion'];
+//     $id_usuario = $_POST['id_usuario'];
+//     $id_perfil = $_POST['id_perfil'];
 
-    $ModeloFundacion->add($nit_fundacion, $id_usuario, $id_perfil);
-} else {
-    header("Location: ../Pages/Fundacion_view.php");
-}
+//     $ModeloFundacion->add($nit_fundacion, $nombre_fundacion, $id_usuario, $id_perfil);
+// } else {
+//     header("Location: ../../view/fundacion/FundacionView.php");
+// }
 ?>
 
 
 <!-- ACTUALIZAR -->
-<?php
-require_once('../Modelo/ProductoModel.php');
-require_once('../../Model/conexion.php');
+<!-- <?php
+// require_once('../../Model/FundacionModel.php');
+// require_once('../../Model/conexion.php');
 
-if ($_POST) {
-    $ModeloProducto = new Productos();
-    $id = $_POST['Id'];
-    $nombre = $_POST['Nombre'];
-    $tipo = $_POST['TipoProducto'];
-    $descripcion = $_POST['Descripcion'];
-    $cantidad = $_POST['Cantidad'];
-    $precio = $_POST['Precio'];
-    $ModeloProducto->update($id, $nombre, $tipo, $descripcion, $cantidad, $precio);
-} else {
-    header("Location: ../../ProductoView.php");
-}
+// if ($_POST) {
+//     $ModeloFundacion = new Fundacion();
+//     $nit_fundacion = $_POST['nit_fundacion'];
+//     $nombre_fundacion = $_POST['nombre_fundacion'];
+//     $id_usuario = $_POST['id_usuario'];
+//     $id_perfil = $_POST['id_perfil'];
+    
+//     $ModeloFundacion->update($nit_fundacion, $nombre_fundacion, $id_usuario, $id_perfil);
+// } else {
+//     header("Location: ../../view/fundacion/FundacionView.php");
+// }
 ?>
 
 
 <!-- ELIMINAR -->
-<?php
-require_once('../Modelo/ProductoModel.php');
+<!-- <?php 
+// require_once('../Modelo/ProductoModel.php');
 
-if ($_POST) {
-    $ModeloProducto = new Productos();
-    $id = $_POST['Id'];
-    $ModeloProducto->delete($id);
-} else {
-    header("Location: ../../ProductoView.php");
-}
+// if ($_POST) {
+//     $ModeloProducto = new Productos();
+//     $id = $_POST['Id'];
+//     $ModeloProducto->delete($id);
+// } else {
+//     header("Location: ../../ProductoView.php");
+// }
 ?>
+<?php
+
+require_once('../../Model/FundacionModel.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $action = $_POST['action'] ?? '';
+    $modelo = new Fundacion();
+
+    if ($action === 'add') {
+        $modelo->add($_POST['nit_fundacion'], $_POST['nombre_fundacion'], $_POST['id_usuario'], $_POST['id_perfil']);
+    } elseif ($action === 'update') {
+        $modelo->update($_POST['nit_fundacion'], $_POST['nombre_fundacion'], $_POST['id_usuario'], $_POST['id_perfil']);
+    } elseif ($action === 'delete') {
+        $modelo->delete($_POST['Id']);
+    } else {
+        header("Location: ../../view/fundacion/FundacionView.php");
+    }
+} else {
+    header("Location: ../../view/fundacion/FundacionView.php");
+}
